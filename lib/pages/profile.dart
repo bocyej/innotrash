cd672f205006e295bd9199bc17e2c8b84a09cc74
Jacob Laurel <jacobkristopherlaurel@gmail.com>
2022-09-08 13:31:59 +0800

    added profile card

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../layouts/profilecard.dart';
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/photos/bg_2.png"),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/photos/bg_2.png"),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: null,
+        child: const ProfileCard(),
+      ), //
     );
   }
 }
