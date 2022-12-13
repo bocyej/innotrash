484cc1449eced38358f37414864b247653b43065
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-14 01:50:01 +0800

    increased appbar size & moved search to actions

--- lib/layouts/mapappbar.dart
+++ lib/layouts/mapappbar.dart
@@ -6,71 +6,77 @@ class MapAppbar extends StatelessWidget with PreferredSizeWidget {
   const MapAppbar({Key? key}) : super(key: key);
 
   @override
-  Size get preferredSize => const Size.fromHeight(60);
+  Size get preferredSize => const Size.fromHeight(80);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       automaticallyImplyLeading: false,
-      title: Container(
-        height: 35,
-        width: 185,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          color: Colors.white,
-        ),
-        child: Row(
-          children: const [
-            Padding(padding: EdgeInsets.only(left: 10)),
-            Icon(
-              Icons.search,
-              color: Colors.black,
-              size: 15,
-            ),
-            Text(
-              ' Find an iNNOTrash near you',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 10,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ],
-        ),
-      ),
       backgroundColor: const Color(0xFF2D2F4E),
       foregroundColor: Colors.white,
       elevation: 0.0,
       actions: <Widget>[
-        IconButton(
-          icon: const Icon(Icons.notifications_none_outlined),
-          tooltip: 'Notifications',
-          onPressed: () {},
-          padding: const EdgeInsets.only(right: 20),
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
+        Padding(
+          padding: const EdgeInsets.only(top: 25, right: 60),
+          child: Container(
+            width: 215,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              color: Colors.white,
+            ),
+            child: Row(
+              children: const [
+                Padding(padding: EdgeInsets.only(left: 10)),
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                  size: 17,
+                ),
+                Text(
+                  ' Find an iNNOTrash near you',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
           ),
-          child: Material(
-            shape: const CircleBorder(),
-            clipBehavior: Clip.hardEdge,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ProfilePage()),
-                );
-              },
-              child: Ink.image(
-                image: const AssetImage('assets/photos/profile_pic.png'),
-                height: 30,
-                width: 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25, right: 25),
+          child: InkWell(
+            child: const Icon(Icons.notifications_none_outlined),
+            onTap: () {},
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25, right: 30),
+          child: Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Material(
+              shape: const CircleBorder(),
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfilePage()),
+                  );
+                },
+                child: Ink.image(
+                  image: const AssetImage('assets/photos/profile_pic.png'),
+                  height: 30,
+                  width: 30,
+                ),
               ),
             ),
           ),
         ),
-        const Padding(padding: EdgeInsets.only(right: 15)),
       ],
     );
   }
